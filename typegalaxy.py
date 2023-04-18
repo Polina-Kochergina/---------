3900c7ab06c34e742bbd977b0571079e64380f8e
Polina Kochergina <kpolina0208@gmail.com>
2023-04-18 20:24:39 +0300

    250*4 img selected

--- typegalaxy.py
+++ typegalaxy.py
@@ -1,4 +1,5 @@
 import csv
+from matplotlib import pyplot as plt
 import numpy as np
 import os
 from urllib.request import urlretrieve
@@ -9,6 +10,9 @@ from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
 from keras.layers import Dropout, BatchNormalization
 from keras.models import Sequential
 from sklearn.model_selection import train_test_split
+
+# import pandas as pd    #  pip install pandas
+# from pathlib import Path
 
 def make_model():
     model = Sequential()
@@ -32,41 +36,43 @@ def make_model():
     metrics=['accuracy'])
     return model
 
-data_EL = []
-data_EDGE = []
-data_ACW = []
-data_CW = []
+# data_EL = []
+# data_EDGE = []
+# data_ACW = []
+# data_CW = []
 
-# print(my_data[0])
-with open('Galaxy.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        # print(len(row))
-        if (float(row['P_EL']) > 0.8):
-            data_EL.append([row['RA'],row['DEC'], row['P_EL'], row['P_EDGE'], row['P_ACW'], row['P_CW']])       
-        elif (float(row['P_EDGE']) > 0.8):
-            data_EDGE.append([row["RA"],row["DEC"], row['P_EL'], row['P_EDGE'], row['P_ACW'], row['P_CW']])
-        elif (float(row['P_ACW']) > 0.8):
-            data_ACW.append([row["RA"],row["DEC"], row['P_EL'], row['P_EDGE'], row['P_ACW'], row['P_CW']])
-        elif (float(row['P_CW']) > 0.8):
-            data_CW.append([row["RA"],row["DEC"], row['P_EL'], row['P_EDGE'], row['P_ACW'], row['P_CW']]) 
+# # print(my_data[0])
+# with open('Galaxy.csv') as csvfile:
+#     reader = csv.DictReader(csvfile)
+#     for row in reader:
+#         # print(len(row))
+#         if (float(row['P_EL']) > 0.8):
+#             data_EL.append([row['RA'],row['DEC'], row['P_EL'], row['P_EDGE'], row['P_ACW'], row['P_CW']])       
+#         elif (float(row['P_EDGE']) > 0.8):
+#             data_EDGE.append([row["RA"],row["DEC"], row['P_EL'], row['P_EDGE'], row['P_ACW'], row['P_CW']])
+#         elif (float(row['P_ACW']) > 0.8):
+#             data_ACW.append([row["RA"],row["DEC"], row['P_EL'], row['P_EDGE'], row['P_ACW'], row['P_CW']])
+#         elif (float(row['P_CW']) > 0.8):
+#             data_CW.append([row["RA"],row["DEC"], row['P_EL'], row['P_EDGE'], row['P_ACW'], row['P_CW']]) 
 
 
-def files_writer(ads, name):
-    with open(f'parsed_{name}.csv', 'w', encoding='utf-8') as file:
-        a_pen = csv.writer(file, delimiter=' ', quoting=csv.QUOTE_NONE, quotechar=' ', escapechar=' ', lineterminator = '\n')
-        a_pen.writerow(('RA', 'DEC'))
-        for ad in ads:
-            # print(ad[0])
-            a_pen.writerow(('nearest:B',ad[0], ad[1], ad[2], ad[3], ad[4], ad[5]))
+# def files_writer(ads, name):
+#     with open(f'parsed_{name}.csv', 'w', encoding='utf-8') as file:
+#         a_pen = csv.writer(file, delimiter=' ', quoting=csv.QUOTE_NONE, quotechar=' ', escapechar=' ', lineterminator = '\n')
+#         a_pen.writerow(('RA', 'DEC'))
+#         for ad in ads:
+#             # print(ad[0])
+#             a_pen.writerow(('nearest:B',ad[0], ad[1], ad[2], ad[3], ad[4], ad[5]))
             
-# сохранение файлов
-files_writer(data_EL, 'EL')
-files_writer(data_EDGE, 'EDGE')
-files_writer(data_ACW, 'ACW')
-files_writer(data_CW, 'CW')
+# # сохранение файлов
+# files_writer(data_EL, 'EL')
+# files_writer(data_EDGE, 'EDGE')
+# files_writer(data_ACW, 'ACW')
+# files_writer(data_CW, 'CW')
 
-print('save file')
+# exit(n )
+
+# print('save file')
 
 list = os.listdir("logd25")
 def Read_RA_DEC(name, dir, file):
@@ -105,8 +111,7 @@ def Read_RA_DEC(name, dir, file):
             scale = listCW[item][2]/1.5
             site = f"https://skyserver.sdss.org/dr14/SkyServerWS/ImgCutout/getjpeg?TaskName=Skyserver.Explore.Image&ra={listCW[item][0]}&dec={listCW[item][1]}&scale={scale}&width=64&height=64"
             urlretrieve(site, f"image_{name}\image_{listCW[item][3]}.png")
-        
-    print(name)
+    # print(name)
            
 # Read_RA_DEC("ACW", 'logd25' , list[1])
 # Read_RA_DEC("CW", 'logd25', list[2])
@@ -138,36 +143,129 @@ def open_norm(dir):
         path = os.path.abspath(file)
         # print(path)
         with Image.open(path) as img:
-            # print(img[0][0])
             arr = np.asarray(img)
-            # print(arr)
-            # print(arr[0][0])
-
             max = arr.max()
-            # print(max)
             arr = arr/max
-            # print('fkhgj')
-            # arr.save(f'{dir}\{file}')
-            # if (os.path.exists(f"image_{name}") == False):
-            #     os.mkdir(f"image_{name}")
-        # reader = csv.reader(csvfile, delimiter='|')
-        # listCW = []
+            
 
 # print("close dir")
+
+img_size = 32
+# data_dir = Path(os.getcwd())
+
+# df = pd.concat([pd.read_csv(f) for f in data_dir.glob("csv\*.csv")], ignore_index=True)
+# df.to_csv("GLObal.csv", index=False)
+data = np.array([])
+labels = []
+
+def h(dir, data, labels, num):
+    name = []
+    radec = []
+    imdir = os.listdir(dir)
+    for k in range(len(imdir)):
+        list = os.listdir(f"{dir}\{imdir[k]}")
+        print("длина list", len(list))
+
+        for item, file in enumerate(list):
+            # print(item)
+            while item < num :  
+                n = file.rstrip(".png")
+                name.append(n.lstrip("image_"))
+
+                with Image.open(f"{dir}\{imdir[k]}\{file}") as img:
+                    arr = np.asarray(img)
+                    arr = np.sum(arr, axis=2)
+                data = np.append(data, arr)
+                break
+    # print(data.shape, "datash1")
+    data = data.reshape(-1, 64, 64)
+    name1 = set(name)
+    
+    print(len(name), "длина name")
+    # print(name[307])
+    print(data.shape)
+    cgi = os.listdir('logd25')
+    pars = os.listdir('csv')
+    print(pars)
+    print(cgi)
+
+
+    print('записали data ')
+    new_name = []
+    for k in range(1,len(cgi)):
+        find_kolvo = 0
+        for i in range(num):
+
+            with open(f'logd25\{cgi[k]}') as csvfile:
+                reader = csv.reader(csvfile, delimiter='|')
+                # print(reader.dialect)
+                for key in reader:
+                    # if (key[1].strip() != name[(k-1)*num+i]) :
+                        # break
+
+                    if (key[0][0] != '!')and(key[1].strip() == name[(k-1)*num+i]):
+
+                        line = key[0].lstrip("nearest:B ")
+                        lineRA, lineDEC = line.split()
+                        radec.append([name[(k-1)*num+i], lineRA, lineDEC])
+                        new_name.append(key[1].strip())
+                        find_kolvo += 1
+                    
+                        break
+
+
+                    # if k :
+                        # print(name[(k-1)*num+i])
+        print(find_kolvo, " это м которое показывает сколько галактик конкретног типа мы отобрали")
+    
+
+
+
+    
+    print('записали ra и dec ')
+    print(len(radec), "radec")
+    # print(radec[307])
+    # print("jjjjj")
+    for k in range(len(pars)):
+        for i in range(num):
+
+                with open(f'csv\{pars[k]}') as csvfile1:
+                    reader = csv.reader(csvfile1, delimiter=' ')
+                    for key in reader: 
+
+                        if (radec[ k*num + i ][2] == key[2]):
+
+                            labels.append([key[3], key[4], key[5], key[6]])
+                                    # print(" я не понимаю((())) ")
+                            break
+
+
+    print('записали вероятности ')
+    print(len(labels))
+    data = data[:len(labels), :, :]
+    print(data.shape)
+
+    return data, labels
+    # print(labels)
+    # print(l, m, ll)
+           
+data , list = h("image", data, labels, 250)    
+# print("получила ли я data и labels", len(labels))                  
+
+labels = np.array(list)
 
 # open_norm('image_ACW')
 # open_norm('image_CW')
 # open_norm('image_EDGE')
 # open_norm('image_EL')
-img_size = 64
+
 
 data_train, data_test, labels_train, labels_test =train_test_split(data, labels, test_size=0.20)
+print(data_train.shape, data_test.shape)
 data_train = data_train.reshape(-1, img_size, img_size, 1)
 data_test = data_test.reshape(-1, img_size, img_size, 1)
 model = make_model()
-model.fit(data_train, labels_train,
-validation_data=(data_test, labels_test),
-epochs=5, batch_size=32)
+model.fit(data_train, labels_train, validation_data=(data_test, labels_test), epochs=5, batch_size=16)
 
 print('DONE!')
 # print(data_EL[0][1])
